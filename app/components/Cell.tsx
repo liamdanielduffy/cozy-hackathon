e3c736a2c9e9948cb8c8946cea87bf7d5163ade2
Andrew Roberts <ajroberts0417@gmail.com>
2024-02-24 13:14:00 -0500

    add state

--- app/components/Cell.tsx
+++ app/components/Cell.tsx
@@ -31,25 +31,32 @@ export default function TextCell({ height }: CellProps) {
 
 
 export function CodeCell({ height }: CellProps) {
-  const [text, setText] = useState('');
+  const [val, setVal] = useState('');
   const [error, setError] = useState(false);
   const [evalResult, setEvalResult] = useState('');
 
   useEffect(() => {
+
+    // step 1 -- get all of the references (sub-vals), which begin with an @ symbol, so get all the @1 or @liam.2 or etc.
+    // step 2 -- fetch those reference's code from global store
+    // step 3 -- replace all of the references with the actual values by evaling them (this happens recursively)
+    // step 4 -- eval this val
+    // step 5 -- return the result of that eval
+
     try {
-      const result = eval(text);
+      const result = eval(val);
       setEvalResult(String(result));
       setError(false);
     } catch {
       setError(true);
     }
-  }, [text]);
+  }, [val]);
 
   return (
     <CellContainer>
       <TextField className={`flex items-center flex-row ${error ? 'text-red-500' : ''}`}>
         <Label>1</Label>
-        <Input className="w-full h-full pl-2" value={text} onChange={(e) => setText(e.target.value)} />
+        <Input className="w-full h-full pl-2" value={val} onChange={(e) => setVal(e.target.value)} />
         {!error && <div>{evalResult}</div>}
         {error && <div>Error</div>}
       </TextField>
