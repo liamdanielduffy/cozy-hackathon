fd3ed0ea7c23127b629d3d95e88e85934cabd3ec
Liam Duffy <liamdanielduffy@gmail.com>
2024-02-24 13:26:07 -0500

    adds firestore

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,22 @@ import { useState } from "react";
 
 import { House } from "~/app/components/House";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBg5Pzcza66C8FlvxVDSz2_injqrOmJf0Q",
+  authDomain: "cozy-hackathon.firebaseapp.com",
+  projectId: "cozy-hackathon",
+  storageBucket: "cozy-hackathon.appspot.com",
+  messagingSenderId: "312548285382",
+  appId: "1:312548285382:web:db25ecc43b49d0878f30c7",
+  measurementId: "G-SGKQY5V0SN"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -14,6 +30,8 @@ export default function Index() {
 
   const [name1State, setName1State] = useState('Hi')
   const [name2State, setName2State] = useState('Hello')
+
+  console.log(db.type);
 
   return (
     <div className="px-8 py-4" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
